5eab94513b3d0417dfc960d943954f3b7a8ba3be
nathnag <99888290+nathnagg@users.noreply.github.com>
2026-04-25 13:20:05 +0100

    Update and rename my-theme.lua to aqua.lua

--- lua/lualine/themes/aqua.lua
+++ lua/lualine/themes/aqua.lua
@@ -1,5 +1,5 @@
-local colorscheme = require 'my-theme.colorscheme'
-local config = require 'my-theme.config'
+local colorscheme = require 'aqua.colorscheme'
+local config = require 'aqua.config'
 local theme = {}
 
 local bg = config.transparent and 'NONE' or colorscheme.editorBackground
